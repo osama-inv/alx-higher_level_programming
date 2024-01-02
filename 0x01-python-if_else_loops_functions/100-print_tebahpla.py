1a550c42910e224d756acfcd595b4d2778a8b92d
osama-inv <osamamahmoudac@gmail.com>
2024-01-02 08:23:59 -0800

    commit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-t = 32
-for i in range(90, 64, -1):
-    print(chr(i+t), end="")
-    t = 32 if t == 0 else 0
+
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
