023e6d1ea3692c2057b8e68585fd6ca8e2fd7bb7
osama-inv <osamamahmoudac@gmail.com>
2024-01-10 14:44:20 -0800

    commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    if not isinstance(roman_string, str):
+        return 0
     theNum = 0
     last = 0
     rank = {'M':1000, 'X':10, 'C':100, 'I':1, 'V':5, 'L':50, 'D':500}
